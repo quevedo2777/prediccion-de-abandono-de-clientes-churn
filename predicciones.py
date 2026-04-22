c8e7a3e5c4b04f71214c10dd9cc8b8db709b7bca
quevedo2777 <quevedo1092347140@gmail.com>
2026-04-22 10:51:26 -0500

    verdadera version base

--- predicciones.py
+++ predicciones.py
@@ -1,9 +1,31 @@
+#parametros de entrada
 cliente={}
-cliente['frecuencia_compra_ultimos_90_dias']=input()
+cliente['compras_ultimos_90_dias']=int(input())
 cliente['RFM']=input()
 cliente['dias_desde_ultima_compra']=input()
-cliente['numero_de_quejas']=input()
+cliente['quejas']=input()
 cliente['interacciones_con_el_servicio_al_cliente']=input()
-cliente['seciones_por_semana']=input()
+cliente['seciones_app']=int(input())
+cliente['dias_sin_compra']=int(input())
 print(cliente)
 
+#base de conocimiento
+
+if cliente['compras_ultimos_90_dias']>90:
+    cliente['riesgo']='alto'
+if cliente['quejas']>2:
+    cliente['status']='insatisfecho'
+if cliente['uso_app']
+
+
+#ingenieria de features
+score_recencia=1/(1+cliente['dias_sin_compra'])
+score_frecuencia=cliente['compras_ultimos_90_dias']/90
+score_monetario=
+score_compromiso=cliente['seciones_app']/7
+score_satisfaccion=1-(cliente['quejas']/cliente['interacciones_con_el_servicio_al_cliente'])
+
+#salida
+print('cliente tiene una probabilidad de: ',posibilidad_de_retirarse)
+print('debe de tomar la siguientes acciones: ',acciones)
+print('valor que se pierde si se deja ir al cliente: ', CLV)
