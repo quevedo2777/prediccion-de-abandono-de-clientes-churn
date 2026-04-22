6d79d11ca51d70ffb83b942454892efd588744e6
quevedo2777 <quevedo1092347140@gmail.com>
2026-04-22 10:46:04 -0500

    version base

---
+++ predicciones.py
@@ -0,0 +1,9 @@
+cliente={}
+cliente['frecuencia_compra_ultimos_90_dias']=input()
+cliente['RFM']=input()
+cliente['dias_desde_ultima_compra']=input()
+cliente['numero_de_quejas']=input()
+cliente['interacciones_con_el_servicio_al_cliente']=input()
+cliente['seciones_por_semana']=input()
+print(cliente)
+
